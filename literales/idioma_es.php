3af108120df7dc543521b08ec0fdf714d7f8f4ba
Aspar3 <asparcao@gmail.com>
2025-10-21 20:21:43 +0200

    Literales

--- literales/idioma_es.php
+++ literales/idioma_es.php
@@ -183,7 +183,7 @@ define("litRespuestaNumericos", 'Numéricos');
 define("litRespuestaSiNoSi", '"Sí" y "No", donde lo que suma es el "Sí"');
 define("litRespuestaSiNoNo", '"Sí" y "No", donde lo que suma es el "No"');
 define("litAyudaRespuesta", '- '.litRespuestaNumericos.': En un reto de quién da más pasos, cada vez que se meten datos se metería el NÚMERO DE PASOS que se han dado.\n\n- '.litRespuestaSiNoSi.': En un reto de quién hace deporte más dias, lo que DA PUNTOS es RESPONDER SÍ a la pregunta "¿Has hecho deporte hoy?"\n\n- '.litRespuestaSiNoNo.': En un reto de quién fuma menos dias, lo que DA PUNTOS es RESPONDER NO a la pregunta "¿Has fumado hoy?"');
-define("litIniciaGoogle", 'Inicia sesión con Google');
-define("litIntroduceDatosAcceso", 'O introduce tus datos de accceso:');
+define("litIniciaGoogle", 'Inicie sesión con Google');
+define("litIntroduceDatosAcceso", 'O introduzca sus datos de accceso:');
 
 ?>
